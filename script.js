74a0e55f0072015437c2f3b52e0f10af6fdc3cc7
Davud Kakhrimanov <hikm.wa.souf@gmail.com>
2017-02-13 23:47:32 +0300

    top caption applied

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $(".meme-gallery img").click(function() {
-    var usePic = $(this).attr("src");
-    $(".meme img").attr("src", usePic);
-})   
+      var usePic = $(this).attr("src");
+      $(".meme img").attr("src", usePic);
     })
+  
+  $("#top-text").keyup(function() {
+      var topText = $("input#top-text").val();
+      $("h1.top-caption").text(topText);
+  })
+})
 
